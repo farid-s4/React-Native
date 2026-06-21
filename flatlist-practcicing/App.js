dd875774eb27c88435ab2014cd4f24e90b749e3a
farid-s4 <faridsadirov4@gmail.com>
2026-06-21 17:21:51 +0400

    fixed flatlist

--- flatlist-practcicing/App.js
+++ flatlist-practcicing/App.js
@@ -1,47 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import NameList from './components/NameList';
-import ProfileCard from './components/ProgileCard';
-import profilePhoto from './assets/profile.jpg';
-import { FlatList } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import NameList from "./components/NameList";
+import ProfileCard from "./components/ProgileCard";
+import profilePhoto from "./assets/profile.jpg";
+import { FlatList } from "react-native";
 
 export default function App() {
   const profileCards = [
-    <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,  
-      <ProfileCard name="John"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
-      <ProfileCard name="John Doe"
-     city="New York"
-      profilePhoto={require('./assets/profile.jpg')} />,
+    { id: 1, name: "cbcvbc", city: "Baku", photo: profilePhoto },
+    { id: 2, name: "xdfs", city: "Moscow", photo: profilePhoto },
+    { id: 3, name: "cvbb", city: "Berlin", photo: profilePhoto },
+    { id: 4, name: "dsd", city: "Amsterdam", photo: profilePhoto },
+    { id: 5, name: "sdf", city: "London", photo: profilePhoto },
+  ];
 
-  ]
   return (
-    <View >
+    <View style={styles.container}>
       <FlatList
-      ListHeaderComponent={<Text style={styles.listHeaderStyle}>Header</Text>}
-      ListEmptyComponent={<Text style={styles.listEmptyStyle}>Empty</Text>}
-      data={profileCards}
-      renderItem={({item})=>console.log(item) || item}
-      keyExtractor={(item, index) => String(index)}
-      contentContainerStyle={styles.container}
+        contentContainerStyle={styles.list}
+        ListHeaderComponent={<Text style={styles.listHeaderStyle}>Header</Text>}
+        ListEmptyComponent={<Text style={styles.listEmptyStyle}>Empty</Text>}
+        data={profileCards}
+        renderItem={({item})=>{
+          console.log(item)
+          return <ProfileCard name={item.name} city={item.city} profilePhoto={item.photo}/>
+        }}
+        keyExtractor={item=>String(item.id)}
       />
     </View>
   );
@@ -49,20 +33,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-     marginTop: 100,
+    paddingTop: 50,
+    flex: 1,
+    backgroundColor: "#fff",
   },
-  listHeaderStyle:{
+  list: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listHeaderStyle: {
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
-  listEmptyStyle:{
+  listEmptyStyle: {
     fontSize: 30,
-    color: '#924a4ade',
-    textAlign: 'center',
-  }
+    color: "#924a4ade",
+    textAlign: "center",
+  },
 });
